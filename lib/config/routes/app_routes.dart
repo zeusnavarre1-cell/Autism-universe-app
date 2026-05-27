e25a67739848199d96c3fdfd1c86fa6401dfeed1
zeusnavarre1-cell <zeusnavarre1@gmail.com>
2026-05-27 22:34:14 +0200

    Add app routes

---
+++ lib/config/routes/app_routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static final Map<String, WidgetBuilder> routes = {
+    '/home': (context) => const Placeholder(),
+    '/games': (context) => const Placeholder(),
+    '/communication': (context) => const Placeholder(),
+    '/routines': (context) => const Placeholder(),
+    '/relax': (context) => const Placeholder(),
+    '/rewards': (context) => const Placeholder(),
+    '/profile': (context) => const Placeholder(),
+    '/register': (context) => const Placeholder(),
+  };
+}
